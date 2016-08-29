24838bab8fe8d79d3b10593fe54dac3398040ad1
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-08-29 11:51:01 -0600

    complete pizza express exercise

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const generateId = require('./lib/generate_id');
 const app = express();
 
+
+app.use(express.static('static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
 app.locals.title = "Pizza Express";
+app.locals.pizzas = {};
 
 app.get('/', (request, response) => {
-  response.send(app.locals.title);
+  response.render('index')
+});
+
+app.post('/pizzas', (request, response) => {
+  if(!request.body.pizza) { return response.sendStatus(400); }
+  var id = generateId();
+
+  app.locals.pizzas[id] = request.body.pizza;
+  response.redirect("/pizzas/" + id);
+})
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+
+  response.render('pizza', {pizza: pizza });
 })
 
 if (!module.parent) {
